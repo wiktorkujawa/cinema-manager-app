71a4717398df7a6079f28d669b8662bcf618953d
wiktor <wiktorkujawa1993@gmail.com>
2020-12-08 16:52:36 +0100

    Added remove showing from hall method

--- src/app/components/Hall/hall-item/hall-item.component.ts
+++ src/app/components/Hall/hall-item/hall-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { HallService } from 'src/app/services/hall.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -11,14 +12,20 @@ export class HallItemComponent implements OnInit {
   @Input() username: any;
   @Output() deleteHall : EventEmitter<any> = new EventEmitter();
   @Output() openUpdateDialog : EventEmitter<any> = new EventEmitter();
+  @Output() deleteMovie : EventEmitter<{hall_id:any, movie_id:any}> = new EventEmitter();
   environment: string = environment.apiUrl;
-  constructor() {}
+  constructor(
+    private hallService: HallService
+  ) {}
 
   
   ngOnInit(): void {
   }
   
 
+  onDeleteFromHall(movie_id: string){
+    this.deleteMovie.emit({hall_id: this.hall._id, movie_id: movie_id});
+  }
   onUpdate(_id:any) {
     this.openUpdateDialog.emit(_id);
   }
