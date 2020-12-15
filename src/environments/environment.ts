edf2085129a0b14a9da82dbdf70e39b5dc9276ed
wiktor <wiktorkujawa1993@gmail.com>
2020-12-15 13:33:11 +0100

    Changed set-env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,1 @@
-export const environment = {
-   rapidApiKey: 'a16322f6admsh01988b356dbb0cfp1d6770jsn9b2faae37767', 
-   production: false,
-   apiUrl: 'http://localhost:4000'
-};
+export const environment = {};
